73b2ae08485622e3127e5302c839a90c82506cb9
Andrew Li <thesorin65@gmail.com>
2017-09-23 14:57:56 -0500

    graph doesnt autoscale

--- src/org/usfirst/frc5427/postSeason2017/GraphPanel.java
+++ src/org/usfirst/frc5427/postSeason2017/GraphPanel.java
@@ -24,15 +24,14 @@ import javax.swing.SwingUtilities;
  *
  * @author Andrew
  */
-public class GraphPanel extends JPanel {
-	public static final int numLines = 2;
+public class GraphPanel extends JPanel
+{
+	public static final int numLines = 1;
 	private static ArrayList<ArrayList<Double>> lines = new ArrayList<ArrayList<Double>>(numLines);
-	private int width = 800;
-	private int heigth = 400;
 	private int padding = 25;
 	private int labelPadding = 25;
 	private Color line1Color = new Color(44, 102, 230, 180);
-	private Color line2Color = new Color(255,51,51,180);
+	private Color line2Color = new Color(255, 51, 51, 180);
 	private Color pointColor = new Color(100, 100, 100, 180);
 	private Color gridColor = new Color(200, 200, 200, 200);
 	private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
@@ -40,14 +39,17 @@ public class GraphPanel extends JPanel {
 	private int numberYDivisions = 10;
 	private List<Double> scores;
 
-	public GraphPanel(List<Double> scores) {
-		this.scores = scores;
-	}
+	private int xMax = 40;
+	private int xMin = 0;
+	private int yMin = 0;
+	private int yMax = 100;
 
-	public static void addLines(int lineNumber, List<Double> linePoints) {
+	public static void addLines(int lineNumber, List<Double> linePoints)
+	{
 		lines.add(lineNumber, (ArrayList<Double>) linePoints);
 		System.out.println("\n\nLine number: " + lineNumber + "\n---------");
-		for (int i = 0; i < linePoints.size(); i++) {
+		for (int i = 0; i < linePoints.size(); i++)
+		{
 			System.out.println(linePoints.get(i));
 		}
 
@@ -55,22 +57,24 @@ public class GraphPanel extends JPanel {
 	}
 
 	@Override
-	protected void paintComponent(Graphics g) {
+	protected void paintComponent(Graphics g)
+	{
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (scores.size() - 1);
-		double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (getMaxScore() - getMinScore());
+		double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (xMax - xMin - 1);
+		double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (yMax - yMin);
 		System.out.print(yScale);
 		// List<Point> graphPoints = new ArrayList<>();
-		System.out.print(getMaxScore());
 		ArrayList<List<Point>> graphPoints = new ArrayList<List<Point>>(lines.size());
-		for (int j = 0; j < lines.size(); j++) {
+		for (int j = 0; j < lines.size(); j++)
+		{
 			graphPoints.add(j, new ArrayList<Point>());
-			for (int i = 0; i < lines.get(j).size(); i++) {
+			for (int i = 0; i < lines.get(j).size(); i++)
+			{
 				int x1 = (int) (i * xScale + padding + labelPadding);
-				int y1 = (int) ((getMaxScore() - lines.get(j).get(i)) * yScale + padding);
+				int y1 = (int) ((yMax - (lines.get(j).get(i))) * yScale + padding);
 				graphPoints.get(j).add(new Point(x1, y1));
 			} // System.out.print(graphPoints.size());
 		}
@@ -81,18 +85,19 @@ public class GraphPanel extends JPanel {
 		g2.setColor(Color.BLACK);
 
 		// create hatch marks and grid lines for y axis.
-		for (int i = 0; i < numberYDivisions + 1; i++) {
+		for (int i = 0; i < numberYDivisions + 1; i++)
+		{
 			int x0 = padding + labelPadding;
 			int x1 = pointWidth + padding + labelPadding;
 			int y0 = getHeight()
 					- ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
 			int y1 = y0;
-			if (scores.size() > 0) {
+			if (lines.get(0).size() > 0)
+			{
 				g2.setColor(gridColor);
 				g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
 				g2.setColor(Color.BLACK);
-				String yLabel = ((int) ((getMinScore()
-						+ (getMaxScore() - getMinScore()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+				String yLabel = ((int) ((yMin + (yMax - yMin) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
 				FontMetrics metrics = g2.getFontMetrics();
 				int labelWidth = metrics.stringWidth(yLabel);
 				g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
@@ -101,13 +106,16 @@ public class GraphPanel extends JPanel {
 		}
 
 		// and for x axis
-		for (int i = 0; i < scores.size(); i++) {
-			if (scores.size() > 1) {
-				int x0 = i * (getWidth() - padding * 2 - labelPadding) / (scores.size() - 1) + padding + labelPadding;
+		for (int i = xMin; i <= xMax; i++)
+		{
+			if (xMax - xMin > 1)
+			{
+				int x0 = i * (getWidth() - padding * 2 - labelPadding) / (xMax - xMin) + padding + labelPadding;
 				int x1 = x0;
 				int y0 = getHeight() - padding - labelPadding;
 				int y1 = y0 - pointWidth;
-				if ((i % ((int) ((scores.size() / 20.0)) + 1)) == 0) {
+				if ((i % ((int) (((xMax - xMin) / 20.0)))) == 0)
+				{
 					g2.setColor(gridColor);
 					g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
 					g2.setColor(Color.BLACK);
@@ -128,23 +136,25 @@ public class GraphPanel extends JPanel {
 		Stroke oldStroke = g2.getStroke();
 		g2.setColor(line1Color);
 		g2.setStroke(GRAPH_STROKE);
-		for (int j = 0; j < lines.size(); j++) {
-			if(j == 0)
+		for (int j = 0; j < lines.size(); j++)
+		{
+			if (j == 0)
 				g2.setColor(line1Color);
 			else
 				g2.setColor(line2Color);
-				
-			System.out.println("\nLine "+(j+1));
+
+			System.out.println("\nLine " + (j + 1));
 			// LINES
-			for (int i = 0; i < graphPoints.get(j).size() - 1; i++) {
+			for (int i = 0; i < graphPoints.get(j).size() - 1; i++)
+			{
 				int x1 = graphPoints.get(j).get(i).x;
-				System.out.println("x1: "+x1);
+				System.out.println("x1: " + x1);
 				int y1 = graphPoints.get(j).get(i).y;
-				System.out.println("y1: "+y1);
+				System.out.println("y1: " + y1);
 				int x2 = graphPoints.get(j).get(i + 1).x;
-				System.out.println("x2: "+x2);
+				System.out.println("x2: " + x2);
 				int y2 = graphPoints.get(j).get(i + 1).y;
-				System.out.println("y2: "+y2 +"\n");
+				System.out.println("y2: " + y2 + "\n");
 				g2.drawLine(x1, y1, x2, y2);
 			}
 			System.out.print("\n\n");
@@ -152,10 +162,12 @@ public class GraphPanel extends JPanel {
 		}
 		g2.setStroke(oldStroke);
 		g2.setColor(pointColor);
-		for (int j = 0; j < lines.size(); j++) {
+		for (int j = 0; j < lines.size(); j++)
+		{
 			// POINT
-		
-			for (int i = 0; i < graphPoints.get(j).size(); i++) {
+
+			for (int i = 0; i < graphPoints.get(j).size(); i++)
+			{
 				int x = graphPoints.get(j).get(i).x - pointWidth / 2;
 				int y = graphPoints.get(j).get(i).y - pointWidth / 2;
 				int ovalW = pointWidth;
@@ -170,27 +182,33 @@ public class GraphPanel extends JPanel {
 	// return new Dimension(width, heigth);
 	// }
 
-	private double getMinScore() {
-		double minScore = Double.MAX_VALUE;
-		for (Double score : lines.get(0)) {
-			minScore = Math.min(minScore, score);
-		}
-		for (Double score : lines.get(1)) {
-			minScore = Math.min(minScore, score);
-		}
-		return minScore;
-	}
+	// private double getMinScore() {
+	// double minScore = Double.MAX_VALUE;
+	// for (Double score : lines.get(0)) {
+	// minScore = Math.min(minScore, score);
+	// }
+	// if(lines.size()>1)
+	// {
+	// for (Double score : lines.get(1)) {
+	// minScore = Math.min(minScore, score);
+	// }
+	// }
+	// return minScore;
+	// }
 
-	private double getMaxScore() {
-		double maxScore = Double.MIN_VALUE;
-		for (Double score : lines.get(0)) {
-			maxScore = Math.max(maxScore, score);
-		}
-		for (Double score : lines.get(1)) {
-			maxScore = Math.max(maxScore, score);
-		}
-		return maxScore;
-	}
+	// private double getMaxScore() {
+	// double maxScore = Double.MIN_VALUE;
+	// for (Double score : lines.get(0)) {
+	// maxScore = Math.max(maxScore, score);
+	// }
+	// if(lines.size()>1)
+	// {
+	// for (Double score : lines.get(1)) {
+	// maxScore = Math.max(maxScore, score);
+	// }
+	// }
+	// return maxScore;
+	// }
 
 	/*
 	 * public void setScores(List<Double> scores) { this.scores = scores;
@@ -199,24 +217,10 @@ public class GraphPanel extends JPanel {
 	/*
 	 * public List<Double> getScores() { return scores; }
 	 */
-	private static void createAndShowGui() {
-		List<Double> scores = new ArrayList<>();
-
-		// TODO: replace values
-		int maxDataPoints = 40;
-		int maxScore = 100;
-		double[] allPoints = new double[numLines * maxDataPoints];
-		for (int j = 0; j < numLines; j++) {
-			scores = new ArrayList<>();
-			for (int i = 0; i < maxDataPoints; i++) {
-				allPoints[j * maxDataPoints + i] = Math.random() * maxScore;
-				scores.add(allPoints[j * maxDataPoints + i]);
-				// scores.add((double) i);
-			}
-			addLines(j, scores);
-		}
-
-		GraphPanel mainPanel = new GraphPanel(scores);
+	private static void createAndShowGui()
+	{
+		addPoints();
+		GraphPanel mainPanel = new GraphPanel();
 		mainPanel.setPreferredSize(new Dimension(800, 600));
 		JFrame frame = new JFrame("DrawGraph");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -226,9 +230,32 @@ public class GraphPanel extends JPanel {
 		frame.setVisible(true);
 	}
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
+	public static void addPoints()
+	{
+		List<Double> scores = new ArrayList<>();
+
+		int maxDataPoints = 40;
+		int maxScore = 100;
+		double[] allPoints = new double[numLines * maxDataPoints];
+		for (int j = 0; j < numLines; j++)
+		{
+			scores = new ArrayList<>();
+			for (int i = 0; i < maxDataPoints; i++)
+			{
+				allPoints[j * maxDataPoints + i] = Math.random() * maxScore;
+				scores.add(allPoints[j * maxDataPoints + i]);
+				// scores.add((double) i);
+			}
+			addLines(j, scores);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
 				createAndShowGui();
 			}
 		});
