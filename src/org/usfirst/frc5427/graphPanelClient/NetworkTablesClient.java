b2a2a53db48d4fa9806eb33dfca212643c5c9ca0
Varsha Kumar <varsha.kumar@outlook.com>
2017-09-23 13:51:16 -0500

    run method

--- src/org/usfirst/frc5427/graphPanelClient/NetworkTablesClient.java
+++ src/org/usfirst/frc5427/graphPanelClient/NetworkTablesClient.java
@@ -3,6 +3,11 @@ package org.usfirst.frc5427.graphPanelClient;
 public class NetworkTablesClient {
 	public static void main (String [] args)
 	{
+		new NetworkTablesClient().run();
+	}
+	
+	public void run()
+	{
 		
 	}
 
