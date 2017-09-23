768399cd553811961b7e722fbb6ef1204df36b31
Varsha Kumar <varsha.kumar@outlook.com>
2017-09-23 14:07:04 -0500

    part of NetworkTableClient

--- src/org/usfirst/frc5427/graphPanelClient/NetworkTablesClient.java
+++ src/org/usfirst/frc5427/graphPanelClient/NetworkTablesClient.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc5427.graphPanelClient;
 
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+//@Author Varsha, Kipp
+
 public class NetworkTablesClient {
 	public static void main (String [] args)
 	{
@@ -8,7 +12,9 @@ public class NetworkTablesClient {
 	
 	public void run()
 	{
-		
+		NetworkTable.setClientMode();
+		NetworkTable.setIPAddress("10.54.27.71");
+		NetworkTable table = NetworkTable.getTable("dataTable");
 	}
 
 }
