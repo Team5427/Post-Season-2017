7b42d359c467542aff6518b3d60449beb7a4f788
Akshat <respectthegame23@gmail.com>
2017-09-29 12:56:08 -0500

    changed y scaling

--- src/org/usfirst/frc5427/postSeason2017/GraphPointsScrollFrame.java
+++ src/org/usfirst/frc5427/postSeason2017/GraphPointsScrollFrame.java
@@ -12,29 +12,39 @@ public class GraphPointsScrollFrame extends JFrame  {
 
 
     JScrollPane sp_textScroller = null;
-    JLabel y_lbl = null;
 
     public GraphPointsScrollFrame()
     {
         
         super("Graph Points Scroll");
-        setSize(1200,700);
+        setSize(1700,700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         sp_textScroller = new JScrollPane(new GraphPointsScrollPanel(600),JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        sp_textScroller.setBounds(150,25,getWidth()-100,getHeight()-70);
-        
-        y_lbl = new JLabel();
-        y_lbl.setIcon(new ImageIcon("src/org/usfirst/frc5427/postSeason2017/yLabels.png"));
-        y_lbl.setBounds(110, -10, 100, 630);
+        sp_textScroller.setBounds(50,25,getWidth()-300,getHeight()-70);
         
         add(sp_textScroller);
-        add(y_lbl);
 
         setVisible(true);
     }
 
-
+    @Override
+    public void paint(Graphics g)
+    {
+    	g.setColor(Color.WHITE);
+    	g.fillRect(0, 0, getWidth(), getHeight());
+    	
+    	g.setColor(Color.BLACK);
+        for(int y = 100; y >= 0; y-=5) {
+<<<<<<< HEAD
+            int ycoo  = (int)((y / 86.0) * (600-90)+ 20);
+=======
+            int ycoo  = (int)((y / 100.0) * (getHeight()-90)+ 20);
+>>>>>>> c0982b8... made y axis NOT BE AN IMAGE AKSHAT U IDIOT
+            g.drawString((100 - y)+"",20,ycoo + 2);
+        }
+        sp_textScroller.repaint();
+    }
 
 
 }
