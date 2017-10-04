3f6b56913c6c1d18292a053251f68cbe927d119a
Varsha Kumar <varsha.kumar@outlook.com>
2017-10-03 19:56:19 -0500

    method bytesToDouble

--- src/org/usfirst/frc5427/graphPanelClient/PackingClass.java
+++ src/org/usfirst/frc5427/graphPanelClient/PackingClass.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc5427.graphPanelClient;
 
+import java.util.Scanner;
+
 public class PackingClass {
 	
 	//Bellow is a list of which bytes mean what in the double word
@@ -8,9 +10,36 @@ public class PackingClass {
 	//ThirdByte:: isRobotRightOfTarget
 	//
 	
-	public  double bytesToDouble(byte b){
-		return 0;
-		
+	public static void main(String[]args)
+	{
+		//Code to test bytesToDouble(b)
+//		Scanner kb= new Scanner(System.in);
+//		int x = 0;
+//		byte[] b = new byte[10];
+//		do
+//		{
+//		System.out.print("Enter the byte index: "+x);
+//		b[x]=kb.nextByte();
+//		x++;
+//		}
+//		while(x<10);
+//		System.out.print("The double is:" + bytesToDouble(b));
 	}
+	
+	
+	public static  double bytesToDouble(byte [] buff){
+		Double d = new Double(0);
+		for(int x=0; x<buff.length;x++)
+		{
+			d += buff[x]*Math.pow(2, x);
+		}
+		return d;
+	}
+	
+	public static byte[] doubleToBytes(double d)
+	{
+		return null;
+	}
+	
 
 }
