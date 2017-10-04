37a88e45e14ff3b6f339224fa25d61bbad311b3a
Varsha Kumar <varsha.kumar@outlook.com>
2017-10-03 19:35:23 -0500

    PackingClass addad

---
+++ src/org/usfirst/frc5427/graphPanelClient/PackingClass.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc5427.graphPanelClient;
+
+public class PackingClass {
+	
+	//Bellow is a list of which bytes mean what in the double word
+	//First Byte:: OnTarget
+	//SecondByte:: isRobotLeftOfTarget
+	//ThirdByte:: isRobotRightOfTarget
+	//
+	
+	public  double bytesToDouble(byte b){
+		return 0;
+		
+	}
+
+}
